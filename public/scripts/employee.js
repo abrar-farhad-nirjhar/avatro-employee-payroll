f75a5e1457c6bc9688d89351ae1038129e257af5
Abrar Farhad <abrar.farhad.af@gmail.com>
2020-03-22 00:20:46 +0600

    final

---
+++ public/scripts/employee.js
@@ -0,0 +1,11 @@
+
+
+class Employee{
+    constructor(id, name, designation, salary){
+        this.id = id
+        this.name = name
+        this.designation = designation
+        this.salary = salary
+
+    }
+}
